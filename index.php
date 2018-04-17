808f339a4a8d4593f20a9585526025c19a481396
Brian Martinez <bmartinez-castaneda@csumb.edu>
2018-04-17 06:26:09 +0000

    Third Push

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if(isset($_POST['itemName'])){
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Products Page</title>
     </head>
-    <body> 
+    <body background="im.png"> 
     <div class='container'>
         <div class='text-center'>
             
--- scart.php
+++ scart.php
@@ -31,7 +31,7 @@ if(isset($_POST['itemId'])) {
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <title>Shopping Cart</title>
     </head>
-    <body>
+    <body background='im.png'>
         <div class='container'>
             <div class='text-center'>
                 
